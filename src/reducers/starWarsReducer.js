6d38ae878e4fa6b560a9c75f64fc788861814d39
Tabita Filimon <tabitafilimon@gmail.com>
2019-03-13 12:20:46 -0400

    initial commit - no errors are popping up but the list of characters are not rendering

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,10 +1,29 @@
-import /* we need our action types here*/ "../actions";
+import { FETCHING, SUCCESS, FAILURE } from  "../actions";
 const initialState = {
-  characters: []
+  characters: [],
   // Array characters, Boolean fetching, null error.
+  fetching: false,
+  error: null
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCHING:
+    return {
+      ...state,
+      fetching: true
+    }
+    case SUCCESS:
+    return {
+      ...state,
+      fetching: false,
+      characters: action.payload
+    }
+    case FAILURE:
+    return {
+      ...state,
+      fetching: false,
+      error: action.payload
+    }
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
